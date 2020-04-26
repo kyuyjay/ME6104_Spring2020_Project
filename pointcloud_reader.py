613c55a7ee6dddf6547428522fdfbb2c2cf3d960
jportillo96 <64325608+jportillo96@users.noreply.github.com>
2020-04-26 18:17:45 -0400

    Commented out code unnecessary to point cloud generation. These blocks were mainly used to visualize the cloud and ensure it was being calculated correctly.

--- pointcloud_reader.py
+++ pointcloud_reader.py
@@ -5,35 +5,35 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
 
-def set_axes_equal(ax):
-    """ Make axes of 3D plot have equal scale so that spheres appear as spheres,
-        cubes as cubes, etc..  This is one possible solution to Matplotlib's
-        ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
-
-        Input:
-            ax: a matplotlib axis, e.g., as output from mplot3d.Axes3D(fig)
-    """
-    # Define Current Axes Limits
-    x_limits = ax.get_xlim3d()
-    y_limits = ax.get_ylim3d()
-    z_limits = ax.get_zlim3d()
-
-    # Calculate Scaling
-    x_range = abs(x_limits[1] - x_limits[0])
-    x_middle = np.mean(x_limits)
-    y_range = abs(y_limits[1] - y_limits[0])
-    y_middle = np.mean(y_limits)
-    z_range = abs(z_limits[1] - z_limits[0])
-    z_middle = np.mean(z_limits)
-
-    # The plot bounding box is a sphere in the sense of the infinity
-    # norm, hence I call half the max range the plot radius.
-    plot_radius = 0.5*max([x_range, y_range, z_range])
-
-    # Set New Axes Limits
-    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
-    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
-    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+# def set_axes_equal(ax):
+#     """ Make axes of 3D plot have equal scale so that spheres appear as spheres,
+#         cubes as cubes, etc..  This is one possible solution to Matplotlib's
+#         ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+#
+#         Input:
+#             ax: a matplotlib axis, e.g., as output from mplot3d.Axes3D(fig)
+#     """
+#     # Define Current Axes Limits
+#     x_limits = ax.get_xlim3d()
+#     y_limits = ax.get_ylim3d()
+#     z_limits = ax.get_zlim3d()
+#
+#     # Calculate Scaling
+#     x_range = abs(x_limits[1] - x_limits[0])
+#     x_middle = np.mean(x_limits)
+#     y_range = abs(y_limits[1] - y_limits[0])
+#     y_middle = np.mean(y_limits)
+#     z_range = abs(z_limits[1] - z_limits[0])
+#     z_middle = np.mean(z_limits)
+#
+#     # The plot bounding box is a sphere in the sense of the infinity
+#     # norm, hence I call half the max range the plot radius.
+#     plot_radius = 0.5*max([x_range, y_range, z_range])
+#
+#     # Set New Axes Limits
+#     ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+#     ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+#     ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
 
 
 def triangle_area_multi(v1, v2, v3):
@@ -67,62 +67,63 @@ def generate_pt_cloud(v1, v2, v3, n):
     res = (v1 * u) + (v2 * v) + (v3 * w)
     return res.astype(np.float32)
 
+
 def convert(filename):
     # Load mesh from STL file
     my_mesh = mesh.Mesh.from_file(filename)
-    size = 3
+    # size = 3
     numpoints = 100
 
-    # Get XYZ coordinates of all vertices and delete duplicate (shared) vertices
-    v_with_dupes = np.vstack((my_mesh.v0, my_mesh.v1, my_mesh.v2))
-    v_unique = np.unique(v_with_dupes, axis=0)
+    # # Get XYZ coordinates of all vertices and delete duplicate (shared) vertices
+    # v_with_dupes = np.vstack((my_mesh.v0, my_mesh.v1, my_mesh.v2))
+    # v_unique = np.unique(v_with_dupes, axis=0)
 
-    # Create a new plot for raw mesh data
-    figure = plt.figure()
-    ax1 = mplot3d.Axes3D(figure)
+    # # Create a new plot for raw mesh data
+    # figure = plt.figure()
+    # ax1 = mplot3d.Axes3D(figure)
 
-    # Add the points from the STL mesh model to the plot
-    X_raw = v_unique[:, 0]
-    Y_raw = v_unique[:, 1]
-    Z_raw = v_unique[:, 2]
-    ax1.scatter(X_raw, Y_raw, Z_raw, c='b', marker='o', s=size)
+    # # Add the points from the STL mesh model to the plot
+    # x_raw = v_unique[:, 0]
+    # y_raw = v_unique[:, 1]
+    # z_raw = v_unique[:, 2]
+    # ax1.scatter(x_raw, y_raw, z_raw, c='b', marker='o', s=size)
 
-    # Auto scale the plot and label axes
-    set_axes_equal(ax1)
-    ax1.set_xlabel('x')
-    ax1.set_ylabel('y')
-    ax1.set_zlabel('z')
+    # # Auto scale the plot and label axes
+    # set_axes_equal(ax1)
+    # ax1.set_xlabel('x')
+    # ax1.set_ylabel('y')
+    # ax1.set_zlabel('z')
 
-    # Show the plot to the screen
+    # # Show the plot to the screen
     # plt.savefig('stl_cloud_raw.png')
     # plt.show()
 
     # Generate point cloud from mesh data
     result_xyz = generate_pt_cloud(my_mesh.v0, my_mesh.v1, my_mesh.v2, numpoints)
 
-    # Save data as pandas DataFrame (not necessary, but data is presented more cleanly)
-    result = pd.DataFrame()
-    result["x"] = result_xyz[:, 0]
-    result["y"] = result_xyz[:, 1]
-    result["z"] = result_xyz[:, 2]
+    # # Save data as pandas DataFrame (not necessary, but data is presented more cleanly)
+    # result = pd.DataFrame()
+    # result["x"] = result_xyz[:, 0]
+    # result["y"] = result_xyz[:, 1]
+    # result["z"] = result_xyz[:, 2]
 
-    # Create new plot for generated point cloud
-    fig2 = plt.figure()
-    ax2 = mplot3d.Axes3D(fig2)
+    # # Create new plot for generated point cloud
+    # fig2 = plt.figure()
+    # ax2 = mplot3d.Axes3D(fig2)
 
-    # Add the points from the cloud to the plot
-    X = result_xyz[:, 0]
-    Y = result_xyz[:, 1]
-    Z = result_xyz[:, 2]
-    ax2.scatter(X, Y, Z, c='r', marker='o', s=size)
+    # # Add the points from the cloud to the plot
+    # x = result_xyz[:, 0]
+    # y = result_xyz[:, 1]
+    # z = result_xyz[:, 2]
+    # ax2.scatter(x, y, z, c='r', marker='o', s=size)
 
-    # Auto scale the plot and label axes
-    set_axes_equal(ax2)
-    ax2.set_xlabel('x')
-    ax2.set_ylabel('y')
-    ax2.set_zlabel('z')
+    # # Auto scale the plot and label axes
+    # set_axes_equal(ax2)
+    # ax2.set_xlabel('x')
+    # ax2.set_ylabel('y')
+    # ax2.set_zlabel('z')
 
-    # Show the plot to the screen
+    # # Show the plot to the screen
     # plt.savefig('stl_cloud_' + str(numpoints) + 'points_' + str(size) + 'markersize.png')
     # plt.show()
 
